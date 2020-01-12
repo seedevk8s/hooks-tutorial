f457c3df1d397d2ec062b769416411f52516d5cb
chjin <seedevk8s@gmail.com>
2020-01-12 20:55:59 +0900

    useState를 여러 번 사용하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import Counter from "./Counter";
+import Info from "./Info";
 
 function App() {
   return (
-    <Counter/>
+    <Info/>
   );
 }
 
